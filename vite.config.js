8bd946161391fe3088730055ce09a0821f911e9c
CesdeUser <gituser@cesde.net>
2025-06-11 10:17:16 -0500

    Fin

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,         // Puerto fijo
+    strictPort: true,   // No permite que Vite cambie el puerto automáticamente
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001", // Puerto donde corre JSON Server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
